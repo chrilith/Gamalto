74cdb1ce27b923d4ead4a8ead2b49070bd1f53af
Chris Apers <chrilith@hotmail.com>
2012-05-05 10:52:14 +0200

    Added some resources managers

---
+++ source/GBaseLibrary.js
@@ -0,0 +1,75 @@
+/*
+ * Gamalto.BaseLibrary
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+(function() {
+
+	/**
+	 * @constructor
+	 */
+	G.BaseLibrary = function(complete) {
+		this._cb = complete;
+		this._pendingCounter = 0;
+		this._list = {};
+	
+		// FIXME: may return true between two loadItem() calls...
+		Object.defineProperty(this, "done", {
+			get: function() { return (this._pendingCounter == 0); },
+			enumerable: true
+		});
+	}
+	
+	/* Inheritance and shortcut */
+	var proto = G.BaseLibrary.inherits(G.Object);
+	
+	proto.getItem = function(name) {
+		return this._list[name];
+	}
+	
+	proto.unloadItem = function(name) {
+		delete this._list[name];
+	}
+	
+	proto.loadItem = function(name, src) {
+		this._pendingCounter++;
+	}
+	
+	proto.hasItem = function(name) {
+		var u;	// undefined
+		return this._list[name] !== u;
+	}
+	
+	proto._done = function() {
+		this._pendingCounter--;
+	}
+
+})();
