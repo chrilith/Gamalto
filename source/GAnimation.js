84c98d2f6cd9a54a4df6a5b701594a9c463a03c3
Chris Apers <chrilith@hotmail.com>
2012-05-05 10:39:39 +0200

    Sprite animator

---
+++ source/GAnimation.js
@@ -0,0 +1,74 @@
+/*
+ * Gamalto.Animation
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+(function() {
+
+	/* Dependencies */
+	G.require	("SpriteSheet");
+	G.using		("Rect");
+	G.using		("Timer");
+
+	/**
+	 * @constructor
+	 */
+	G.Animation = function(bitmap, r, count, tw, th) {
+		G.Object.base(this, r, count, tw, th);
+		this._curr = 0;
+	//	this._speed
+	}
+	
+	/* Inheritance and shortcut */
+	var proto = G.Animation.inherits(G.SpriteSheet);
+	
+	proto.setDuration = function(time) {
+		this._speed = 1 / (time / this.length);
+	}
+	
+	proto.update = function(timer, frame) {
+		var u, o = this,
+			l = o.length;
+	
+		frame = G.getDef(frame, o._curr) || 0;
+		if ((frame += timer.elapsedTime * o._speed) >= l) {
+			frame -= l;
+		}
+	
+		return (o._curr = frame);	// Do not round to keep the fractionnal part 
+	}
+	
+	proto.draw = function(renderer, x, y, i) {
+		i = G.getDef(i, this._curr) || 0;	// Here we change the reference and so arguments[3] is changed
+		G.Animation.base.draw.apply(this, arguments);
+	}
+
+})();
