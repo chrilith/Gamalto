93207c48e3976af26c5fe6e74cfb5a2c4dbc217d
Chris Apers <chrilith@hotmail.com>
2012-06-04 20:09:57 +0200

    Updated debug file

--- source/gamalto.debug.js
+++ source/gamalto.debug.js
@@ -75,6 +75,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		<script src="' + dir + 'GCommandLine.js"></script> \
 		<script src="' + dir + 'GSoundPool.js"></script> \
 		<script src="' + dir + 'GSound.js"></script> \
+		<script src="' + dir + 'GReadableStream.js"></script> \
+		<script src="' + dir + 'GMemoryStream.js"></script> \
 		<script src="' + dir + 'GFile.js"></script> \
 		<script src="' + dir + 'GConvert.js"></script> \
 \
