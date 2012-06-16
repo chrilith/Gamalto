d0e672530c063926f0a1a484be98cee894d80f43
Chris Apers <chrilith@hotmail.com>
2012-06-16 13:00:03 +0200

    Added Canvas object

--- source/gamalto.debug.js
+++ source/gamalto.debug.js
@@ -44,6 +44,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		<script src="' + dir + 'Extensions.js"></script> \
 		<script src="' + dir + 'Missing.js"></script> \
 	\
+		<script src="' + dir + 'GCanvas.js"></script> \
 		<script src="' + dir + 'GSectionList.js"></script> \
 		<script src="' + dir + 'GSpriteSheet.js"></script> \
 		<script src="' + dir + 'GAnimation.js"></script> \
