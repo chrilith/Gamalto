325622ac7f43ac54c059ea43868b58177738500e
Chris Apers <chrilith@hotmail.com>
2012-06-22 20:44:11 +0200

    Added preliminary indexed image support

---
+++ source/GIndexedImage.js
@@ -0,0 +1,181 @@
+/*
+ * Gamalto.IndexedImage
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+// TODO: optimize memory and speed
+
+(function(env) {
+
+	/* Dependencies */
+	G.using("Canvas");
+	G.using("File");
+	G.using("MemoryStream");
+	G.using("Palette");
+
+	/* Local */
+	var decoders = [];
+
+	/**
+	 * @constructor
+	 */
+	G.IndexedImage = function() {
+		this._file = new G.File();
+		
+		Object.defineProperty(this, "src", {
+			set: function(value) {
+				this._url = value;
+				this._load();
+			},
+			get: function() {
+				return this._url || "";
+			},
+			enumerable: true
+		});
+	}
+
+	var stat = G.IndexedImage;
+
+	stat._addDecoder = function(decoder) {
+		decoders.push(decoder);
+	}
+
+	/* Inheritance and shortcut */
+	var proto = G.IndexedImage.inherits(G.Object);
+
+	proto._findDecoder = function() {
+		var i, u = this._url,
+			p1 = u.indexOf('?'),
+			p2 = u.indexOf('#');
+			
+		// Get end of filename
+		u = u.substr(0,
+				p1 != -1 ? p1 :
+				p2 != -1 ? p2 : u.length);
+		
+		// Get extention if any
+		p1 = u.lastIndexOf("/");
+		p2 = u.lastIndexOf(".");
+		u = u.substr(p2 > p1 ? p2 : u.length).toLowerCase();
+
+		// Decoder lookup
+		for (i = 0; i < decoders.length; i++) {
+			if (decoders[i].ext.indexOf(u) != -1 &&
+				decoders[i].mime == this._file.mimeType) {
+				
+				return decoders[i].reader;
+			}
+		}
+		return null;
+	}
+
+	proto._load = function() {
+		var file = this._file;
+
+		// Open the file and get info
+		file.open(this._url);
+		
+		// Find the proper decoder
+		if ((this._decoder = this._findDecoder())) {
+
+			// Allocate temporary buffer
+			var buf = new G.MemoryStream(file.length);
+	
+			// Read data
+			file._beginRead(buf, 0,
+							file.length,
+							this._ended.bind(this));
+		} else {
+			this._error();
+		}
+	}
+	
+	proto._error = function() {
+		// TODO
+		if (this.onerror) {
+			this.onerror();
+		}
+	}
+
+	proto._ended = function(buffer, result) {
+		buffer.seek(0);
+
+		var dec  = this._decoder;
+			data = dec ? dec.call(this, buffer) : null;
+
+		this._file._endRead(result);
+		this._file.close();
+
+		if (!data) {
+			this._data = null;
+			this._error();
+
+		} else {
+			this._palette	= data[0];
+			this._data		= data[1];
+			this.width		= data[2];
+			this.height		= data[3];
+	
+			// TODO: use setSize()?
+			this._image =
+				(this._canvas = new G.Canvas(data[2], data[3]))
+					._createRawBuffer();
+	
+			// TODO
+			if (this.onload) {
+				this.onload();
+			}
+		}
+	}
+
+	proto._setPixel = function(p, color) {
+		var data = this._image.data,
+			c = this._palette.getColor(color);
+
+		p <<= 2;
+		data[p + 0] = c.r;
+		data[p + 1] = c.g;
+		data[p + 2] = c.b;
+		data[p + 3] = c.a;
+	}
+
+	proto._toCanvas = function() {
+		var all = this.width * this.height,
+			pix = 0;
+
+		this._data.seek(0);
+		do { this._setPixel(pix++, this._data.readByte()); } while (pix < all);
+
+		this._canvas._copyRawBuffer(this._image);
+		return this._canvas._canvas;
+	}
+
+})(ENV);
---
+++ source/GIndexedImage_AtariDegasElite.js
@@ -0,0 +1,126 @@
+/*
+ * Gamalto.IndexedImage_AtariDegasElite
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+// TOOD: Only PI1 files for now
+
+(function() {
+	/* Dependencies */
+	G.require	("IndexedImage");
+	G.using		("MemoryStream");
+	G.using		("Palette");
+	G.using		("Color");
+
+	/* Local */
+	var base = G.IndexedImage;
+
+	base._addDecoder({
+		mime	: "application/octet-stream",
+		ext		: [".pi1"],
+		reader	: decoder
+	});
+
+	function decoder(buffer) {
+		var palette, data, width, height, x, y;
+	
+		// Not low resolution?
+		if (buffer.readUInt16BE() != 0) {
+			return null;
+		} else {
+			width = 320;
+			height = 200;
+		}
+
+		// Get palette
+		palette = new G.Palette();
+		for (x = 0; x < 16; x++) {
+			var o = buffer.readUInt16BE();
+				var b = ((o >> 0) & 0x7) << 5,
+					g = ((o >> 4) & 0x7) << 5,
+					r = ((o >> 8) & 0x7) << 5;
+
+			palette.addColor(new G.Color(r, g, b));
+		}
+
+		// Image data
+		data = new G.MemoryStream(width * height);
+		for (y = 0; y < height; y++) {
+			for (x = 0; x < width / 16; x++) {
+				var p1 = buffer.readUInt16BE(),
+					p2 = buffer.readUInt16BE(),
+					p3 = buffer.readUInt16BE(),
+					p4 = buffer.readUInt16BE();
+
+				for (b = 0; b < 16; b++) {
+					var d = 15 - b,
+						b1 = (p1 & (1 << d)) >> d,
+						b2 = (p2 & (1 << d)) >> d,
+						b3 = (p3 & (1 << d)) >> d,
+						b4 = (p4 & (1 << d)) >> d,
+
+						pix = (b1 << 0) | (b2 << 1) | (b3 << 2) | (b4 << 3);
+
+					data.writeByte(pix);
+				}
+			}
+		}
+
+		// Palette animations if any
+		if (!buffer.eos()) {
+			var pos  = buffer.tell(),
+				info = []; /* 0 = start color
+						      1 = end color
+						      2 = direction (0 = left, 1 = none, 2 = right)
+						      3 = delay
+						   */
+	
+			y = 0; do {
+				buffer.seek(pos + y * 2);
+	
+				x = 0; do {
+					info[x] = buffer.readUInt16BE();
+					buffer.seek(8 - 2, G.Stream.SEEK_CUR);
+				} while (++x < 4);
+	
+				if (info[2] != 1) {
+					palette.addAnimator(
+						info[2] == 0 ? info[0] : info[1],
+						info[2] == 0 ? info[1] : info[0],
+						(128 - info[3]) / 60 * 1500);	// 1500 to adjust perceived speed
+				}
+			} while (++y < 4);
+		}
+
+		return [palette, data, width, height];
+	}	
+	
+})();
