3acd4d97ff3f5024fb1e27b27c5eb5d6c548753e
Chris Apers <chrilith@hotmail.com>
2012-07-22 19:45:33 +0200

    Added "implements" and "genericize" to test inheritance and add support for generic object respectively

--- source/Extensions.js
+++ source/Extensions.js
@@ -37,18 +37,58 @@ Object.defineMethod(Object.prototype, "is", function(o) {
 	return this instanceof o;
 });
 
+Object.defineMethod(Object.prototype, "implements", function(o) {
+	return this == o || this.prototype instanceof o;
+});
+
 Object.defineMethod(Function.prototype, "inherits", function(baseClass) {
+	baseClass = (baseClass._generic || baseClass);
+
 	this.__base__  = baseClass;
 	this.__super__ = baseClass.prototype;
 
-	var inheritance = function() {};
-	this.base = inheritance.prototype = this.__super__;
+	function ctor() {};
+	this.base = ctor.prototype = this.__super__;
 
-	this.prototype = new inheritance();
-	this.prototype.constructor = this;
+	(this.prototype = new ctor()).constructor = this;
 
 	return this.prototype;
 });
+
+(function() {
+	var types = [];
+		
+	function create(type, base) {
+		if (!type) {
+			throw TypeError();
+		}
+
+		for (var i = 0; i < types.length; i++) {
+			if (types[i].base == base && types[i].T == type) {
+				return types[i].ctor;
+			}
+		}
+	
+		var generic = function() {
+			this._T = type;
+			base.apply(this, Array.prototype.slice.call(arguments, 0));
+		};
+		generic.inherits(base);
+		types.push({ base: base, T: type, ctor: generic });
+
+		return generic;
+	}
+
+	Object.defineMethod(Function.prototype, "genericize", function(base) {
+		var that = this,
+			master = function(T) {
+				return create(T, that);
+			};
+		master._generic = that;
+		return master;
+	});
+
+})();
 
 /* Extra Math methods */
 
