b6edf9dbd7336d945f8835b875f7bbf36175de81
Chris Apers <chrilith@hotmail.com>
2012-10-20 20:02:29 +0200

    Added a base object for shapes

---
+++ source/GShape.js
@@ -0,0 +1,57 @@
+/*
+ * Gamalto.Shape
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+(function() {
+
+	/**
+	 * @constructor
+	 */
+	G.Shape = new Function();
+
+	var proto = G.Shape.inherits(G.Object);
+
+	proto.vectorInShape = function(v) {
+		return this.pointInShape(v.x, v.y);
+	}
+
+	var constant = G.Shape;
+
+	constant.ALIGN_LEFT		= 1 << 0;
+	constant.ALIGN_RIGHT	= 1 << 1;
+	constant.ALIGN_TOP		= 1 << 2;
+	constant.ALIGN_BOTTOM	= 1 << 3;
+
+	constant.ALIGN_CENTER = (1 << 0 | 1 << 1); 	// LEFT+RIGHT
+	constant.ALIGN_MIDDLE = (1 << 2 | 1 << 3);	// TOP+BOTTOM
+
+})();
