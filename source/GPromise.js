571c84bf6e9432c4ef95c9dde1811fa5b91ac8f1
Chris Apers <chrilith@hotmail.com>
2012-10-28 12:51:08 +0100

    Added preliminary promise support
    Will be used later to libraries to ease async management

---
+++ source/GPromise.js
@@ -0,0 +1,128 @@
+/*
+ * Gamalto.Promise
+ * 
+ * This file is part of the Gamalto middleware
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this middleware.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+(function() {	
+	
+	var STATE_PENDING	= 0,
+		STATE_PROGRESS	= 1,
+		STATE_RESOLVED	= 2,
+		STATE_REJECTED	= 3/*,
+		STATE_CANCELED	= 4 TODO ?*/;
+
+	/**
+	 * @constructor
+	 */
+	G.Promise = function() {}
+	
+	/* Inheritance and shortcut */
+	var proto = G.Promise.inherits(G.Object);
+	
+	proto.resolve = function(value) {
+		this._complete(STATE_RESOLVED, value);
+	}
+		
+	proto.reject = function(value) {
+		this._complete(STATE_REJECTED, value);
+	}
+
+	proto.progress = function(value) {
+		var func = this._progress;
+		if (func) { func.apply(func, value); }
+	}
+	
+	proto._complete = function(state, value) {
+		var func;
+
+		if (this._state > STATE_PROGRESS) {
+			return;
+		}
+		this._state = state;
+		this.value = value;
+
+		switch (state) {
+			case STATE_RESOLVED:
+				func = this._resolve;
+				break;
+			case STATE_REJECTED:
+				func = this._reject;
+				break;
+		}
+
+		if (func) { func(value); }
+	}
+
+	proto.then = function(resolve, reject, progress) {
+		var undef, promise = new G.Promise();
+
+		this._resolve = function(value) {
+			value = !resolve ? undef : resolve(value);
+			try {
+				promise.resolve(value);
+			 } catch(e) {
+				promise.reject(e);
+			 }
+		}
+		this._reject = function(value) {
+			value = !reject ? undef : reject(value);
+			promise.reject(value);
+		}
+		this._progress = progress;
+
+		return promise;
+	}
+	
+	G.Promise.all = function(/* args */) {
+		var all = Array.prototype.slice.call(arguments, 0),
+			count = 0,
+			results = [];
+			promise = new G.Promise();
+
+		all.forEach(function(value, i) {
+			all[i].then(
+				function(value) {
+					results[i] = value;
+					promise.progress(count++ / all.length);
+					if (count == all.length) {
+						promise.resolve(results);
+					}
+				},
+				function(error) {
+					promise.reject(error);
+				}
+			);
+		});
+		
+		return promise;
+	}
+	
+})();
