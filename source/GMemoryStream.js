6bcab40f87a92832824653c6e33d70b0ff50880f
Chris <chrilith@hotmail.com>
2013-04-16 21:03:08 +0200

    Way to get a pointer to a stream position

--- source/GMemoryStream.js
+++ source/GMemoryStream.js
@@ -34,7 +34,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 (function() {
 
 	/* Dependencies */
-	gamalto.require("ReadableStream");
+	gamalto.require("ReadStream");
 
 	/**
 	 * @constructor
@@ -44,10 +44,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	}
 
 	/* Inheritance and shortcut */
-	var proto = G.MemoryStream.inherits(G.ReadableStream);
+	var proto = G.MemoryStream.inherits(G.ReadStream);
 	
 	proto.writeByte = function(data) {
-		this._data[this._position++] = String.fromCharCode(data & 0xff);
+		this._data[this._startAt + this._position++] = String.fromCharCode(data & 0xff);
 	}
 
 	/* Big Endian */
