20b0788ab8d02eb26dd9916628b3a2736c3d2610
Chris <chrilith@hotmail.com>
2013-05-11 21:36:16 +0200

    Useless, alreadydone in _writeByte() if needed

--- source/GMemoryStream.js
+++ source/GMemoryStream.js
@@ -78,32 +78,32 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 	proto.writeInt16BE = function(data, at) {
 		at = this._at(at);
-		this._writeByte((data >> 8) & 0xff, at + 0);
-		this._writeByte((data     ) & 0xff, at + 1);
+		this._writeByte((data >> 8), at + 0);
+		this._writeByte((data     ), at + 1);
 	}
 
 	proto.writeInt32BE = function(data, at) {
 		at = this._at(at);
-		this._writeByte((data >> 24) & 0xff, at + 0);
-		this._writeByte((data >> 16) & 0xff, at + 1);
-		this._writeByte((data >>  8) & 0xff, at + 2);
-		this._writeByte((data      ) & 0xff, at + 3);
+		this._writeByte((data >> 24), at + 0);
+		this._writeByte((data >> 16), at + 1);
+		this._writeByte((data >>  8), at + 2);
+		this._writeByte((data      ), at + 3);
 	}	
 
 	/* Little Endian (JavaScript is little endian) */
 	
 	proto.writeInt16LE = function(data, at) {
 		at = this._at(at);
-		this._writeByte((data     ) & 0xff, at + 0);
-		this._writeByte((data >> 8) & 0xff, at + 1);
+		this._writeByte((data     ), at + 0);
+		this._writeByte((data >> 8), at + 1);
 	}
 	
 	proto.writeInt32LE = function(data, at) {
 		at = this._at(at);
-		this._writeByte((data      ) & 0xff, at + 0);
-		this._writeByte((data >>  8) & 0xff, at + 1);
-		this._writeByte((data >> 16) & 0xff, at + 2);
-		this._writeByte((data >> 24) & 0xff, at + 3);
+		this._writeByte((data      ), at + 0);
+		this._writeByte((data >>  8), at + 1);
+		this._writeByte((data >> 16), at + 2);
+		this._writeByte((data >> 24), at + 3);
 	}
 
 	proto.writeString = function(str, stopChar) {
