7de43d96272ac4eb953643260693699ff2884431
Chris <chrilith@hotmail.com>
2013-10-09 20:02:33 +0200

    Changed coding convensions

--- source/GAsync.js
+++ source/GAsync.js
@@ -34,7 +34,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 (function() {
 
 	/* Dependencies */
-	gamalto.require("Promise");
+	gamalto.require_("Promise");
 
 	/**
 	 * @constructor
