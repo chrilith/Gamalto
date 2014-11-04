45803456a799563f8c8145444e48a53423bdcb55
Chris Apers <chrilith@hotmail.com>
2014-11-04 10:56:16 +0100

    Corrected filename

---
+++ source/Environment.js
@@ -0,0 +1,50 @@
+/*
+ * Gamalto Environment Object
+ * 
+ * This file is part of the Gamalto framework
+ * http://www.gamalto.com/
+ *
+
+Copyright (C)2012-2014 Chris Apers and The Gamalto Project, all rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+For production software, the copyright notice only is required. You must also
+display a splash screen showing the Gamalto logo in your game of other software
+made using this Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ *
+ */
+
+(function() {
+
+	var Environment = function() {
+		// This is supposed to be always accessible
+		this.isHttpRangesSupported = false;	// false as of v1.4.1 of CocoonJS
+		this.isMobile = false;				// TODO
+		this.hasAudio = false;				// Will be iniitalized by the audio lib
+	}
+
+	Environement.prototype.init = function() {
+		// TODO
+		return this;
+	}
+
+	gamalto.env = new Environment().init();
+
+})();
