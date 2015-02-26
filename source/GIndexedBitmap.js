04e6d4e580b8c6f67bbe0eeed3c334e55bbae49a
Chris Apers <chrilith@hotmail.com>
2015-02-26 20:29:57 +0100

    Added missing method used by the new BitmapLibrary

--- source/GIndexedBitmap.js
+++ source/GIndexedBitmap.js
@@ -67,4 +67,8 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		return this._source._getCanvas(refresh);	
 	}
 
+	proto._getSourceType = function() {
+		return G.IndexedImage;
+	}
+
 })();
