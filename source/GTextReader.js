2a9e4f69effc95b5f5d356b5fe6be536910a2566
Chris Apers <chrilith@hotmail.com>
2015-03-07 09:05:06 +0100

    Matched binary file requirements only
    Optimized to prevent split().
    In that case TextWriter doesn’t allow direct string modification as it
    is immutable.

--- source/GTextReader.js
+++ source/GTextReader.js
@@ -9,19 +9,12 @@
 (function() {
 
 	var _Object = G.TextReader = function(buffer) {
-		Object.base(this,
-			typeof buffer == "string"
-				? buffer.split('')
-				: buffer);
+		Object.base(this, buffer);
 	},
 	_proto = _Object.inherits(G.ArrayReader);
 
 	_proto._readByte = function(byteOffset) {
-		return this.buffer[byteOffset].charCodeAt(0) & 0xff;
-	};
-
-	_proto.valueOf = function() {
-		return this.buffer.join('');
+		return this.buffer.charCodeAt(byteOffset) & 0xff;
 	};
 
 })();
