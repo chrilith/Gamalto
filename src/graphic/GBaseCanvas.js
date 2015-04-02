2ee6407ebad3924d8a8cb92f77eff879e1057d73
Chris Apers <chrilith@hotmail.com>
2015-04-02 22:11:46 +0200

    A bit more doc

--- src/graphic/GBaseCanvas.js
+++ src/graphic/GBaseCanvas.js
@@ -31,43 +31,62 @@ THE SOFTWARE.
 
 (function() {
 
+	/**
+	 * Abstract object to create a drawing canvas.
+	 *
+	 * @bastract
+	 * 
+	 * @memberof Gamalto
+	 * @constructor Gamalto.BaseCanvas
+	 * @augments Gamalto.Object
+	 */
 	var _Object = G.BaseCanvas = function(width, height) {
 		this._initCanvas();
 		this._setSize(width, height);
 	},
-	_proto = _Object.inherits(G.Object);
+
+	/** @alias Gamalto.BaseCanvas.prototype */
+	proto = _Object.inherits(G.Object);
 
 	_proto._setSize = function(width, height) {
 		var ctx, canvas = this.__canvas;
-		canvas.width  = this.width  = +width  || 0;
-		canvas.height = this.height = +height || 0;
+		canvas.width  = this.width  = Number(width)  || 0;
+		canvas.height = this.height = Number(height) || 0;
 	};
 
-	_proto.createRenderer = function() { };
+	/**
+	 * Creates a renderer for the current canvas type.
+	 *
+	 * @function createRenderer
+	 * @memberof Gamalto.BaseCanvas.prototype
+	 * @abstract
+	 *
+	 * @return {Gamalto.BaseRenderer} Object instance implementing {@link Gamalto.BaseRenderer}.
+	 */
 
-	_proto.hasContext = function() {
+	proto.hasContext = function() {
 		return !!this._getContext();
 	};
 
-	_proto._initCanvas = function() {
+	proto._initCanvas = function() {
 		/* This will never change */
 		this.__canvas = document.createElement("canvas");	
 	};
 
-	_proto.getCanvas_ = function() {
+	proto.getCanvas_ = function() {
 		return this.__canvas;
 	};
 
-	_proto._getContext = function() {
+	proto._getContext = function() {
 		return this._context;
 	};
 
-	_proto._getRawBuffer = function() { };
+	proto._getRawBuffer = function() { };
 
-	_proto._createRawBuffer = function() { };
+	proto._createRawBuffer = function() { };
 
-	_proto._copyRawBuffer = function(raw, x, y) { };
+	proto._copyRawBuffer = function(raw, x, y) { };
 
-	_proto._copyRawBufferIndexed = function(palette, raw, x, y) { };
+	proto._copyRawBufferIndexed = function(palette, raw, x, y) { };
 
 })();
