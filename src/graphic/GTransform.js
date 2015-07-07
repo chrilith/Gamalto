350e8481d8e692884bd001c61f11a2887e573d67
Chris Apers <chrilith@hotmail.com>
2015-07-07 20:20:32 +0200

    Doc

--- src/graphic/GTransform.js
+++ src/graphic/GTransform.js
@@ -45,26 +45,70 @@ THE SOFTWARE.
 	/** @alias Gamalto.Transform.prototype */
 	var proto = _Object.inherits(G.Object);
 
+	/**
+	 * Copies a tranformation state into the current object.
+	 *
+	 * @param  {Gamalto.Transform} transform
+	 *         Object to be copied.
+	 */
 	proto.copy = function(transform) {
 		this.flipX = transform.flipX;
 		this.flipY = transform.flipY;
 		this.alpha = transform.alpha;
 	};
 
+	/**
+	 * Saves the current transformation state.
+	 */
 	proto.save = function() {
+		/**
+		 * Saved transformation state.
+		 *
+		 * @member {Gamalto.Transform}
+		 * @ignore
+		 */
 		this.state_ = this.clone();
 	};
 
+	/**
+	 * Restores the previously saved state.
+	 */
 	proto.restore = function() {
-		this.state_ = this.copy(this.state_);
+		gamalto.devel.assert(this.state_);
+		this.state_ = this.copy(this.state_);	// Reset previous state
 	};
 
 	proto.reset = function() {
+		/**
+		 * Whether to flip horizontally the next drawn element.
+		 *
+		 * @member {boolean}
+		 * @alias Gamalto.Transform#flipX
+		 */
 		this.flipX = false;
+
+		/**
+		 * Whether to flip vertically the next drawn element.
+		 *
+		 * @member {boolean}
+		 * @alias Gamalto.Transform#flipY
+		 */
 		this.flipY = false;
+
+		/**
+		 * Transparency level of the next drawn element.
+		 *
+		 * @member {number}
+		 * @alias Gamalto.Transform#alpha
+		 */
 		this.alpha = 1;
 	};
 
+	/**
+	 * Clones the object.
+	 *
+	 * @return {Gamalto.Transform} Copy of the current object.
+	 */
 	proto.clone = function() {
 		var clone = new _Object();
 		clone.copy(this);
